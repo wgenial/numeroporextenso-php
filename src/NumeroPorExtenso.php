8710532534381a11c940d196b02fe7e56c4beaad
Giovani Generali <giovani@wgenial.com.br>
2021-09-12 19:07:08 -0300

    fix typo

--- src/NumeroPorExtenso.php
+++ src/NumeroPorExtenso.php
@@ -21,7 +21,7 @@
       $plural = array("centavos", "reais", "mil", "milhões", "bilhões", "trilhões","quatrilhões");
       $c = array("", "cem", "duzentos", "trezentos", "quatrocentos","quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos");
       $d = array("", "dez", "vinte", "trinta", "quarenta", "cinquenta","sessenta", "setenta", "oitenta", "noventa");
-      $d10 = array("dez", "onze", "doze", "treze", "quatorze", "quinze","dezesseis", "dezesete", "dezoito", "dezenove");
+      $d10 = array("dez", "onze", "doze", "treze", "quatorze", "quinze","dezesseis", "dezessete", "dezoito", "dezenove");
       $u = array("", "um", "dois", "três", "quatro", "cinco", "seis","sete", "oito", "nove");
 
       $z = 0;
